b0e5504fe62c8d9152f35598fb3872fbf1676e48
xythyl <xxkillx3rxx@gmail.com>
2026-02-27 00:55:09 -0800

    finished systick section

---
+++ 11_systick_delay/Debug/Src/main.d
@@ -0,0 +1,21 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/Xx-M4/OneDrive/Documents/Programming\ Projects/STM32/STM32F466/STM32F4-BareMetal_Workspace/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/Xx-M4/OneDrive/Documents/Programming\ Projects/STM32/STM32F466/STM32F4-BareMetal_Workspace/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h \
+ C:/Users/Xx-M4/OneDrive/Documents/Programming\ Projects/STM32/STM32F466/STM32F4-BareMetal_Workspace/chip_headers/CMSIS/Include/core_cm4.h \
+ C:/Users/Xx-M4/OneDrive/Documents/Programming\ Projects/STM32/STM32F466/STM32F4-BareMetal_Workspace/chip_headers/CMSIS/Include/cmsis_version.h \
+ C:/Users/Xx-M4/OneDrive/Documents/Programming\ Projects/STM32/STM32F466/STM32F4-BareMetal_Workspace/chip_headers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/Xx-M4/OneDrive/Documents/Programming\ Projects/STM32/STM32F466/STM32F4-BareMetal_Workspace/chip_headers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/Xx-M4/OneDrive/Documents/Programming\ Projects/STM32/STM32F466/STM32F4-BareMetal_Workspace/chip_headers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/Xx-M4/OneDrive/Documents/Programming\ Projects/STM32/STM32F466/STM32F4-BareMetal_Workspace/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ ../Inc/uart.h ../Inc/adc.h ../Inc/systick.h
+C:/Users/Xx-M4/OneDrive/Documents/Programming\ Projects/STM32/STM32F466/STM32F4-BareMetal_Workspace/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+C:/Users/Xx-M4/OneDrive/Documents/Programming\ Projects/STM32/STM32F466/STM32F4-BareMetal_Workspace/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h:
+C:/Users/Xx-M4/OneDrive/Documents/Programming\ Projects/STM32/STM32F466/STM32F4-BareMetal_Workspace/chip_headers/CMSIS/Include/core_cm4.h:
+C:/Users/Xx-M4/OneDrive/Documents/Programming\ Projects/STM32/STM32F466/STM32F4-BareMetal_Workspace/chip_headers/CMSIS/Include/cmsis_version.h:
+C:/Users/Xx-M4/OneDrive/Documents/Programming\ Projects/STM32/STM32F466/STM32F4-BareMetal_Workspace/chip_headers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/Xx-M4/OneDrive/Documents/Programming\ Projects/STM32/STM32F466/STM32F4-BareMetal_Workspace/chip_headers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/Xx-M4/OneDrive/Documents/Programming\ Projects/STM32/STM32F466/STM32F4-BareMetal_Workspace/chip_headers/CMSIS/Include/mpu_armv7.h:
+C:/Users/Xx-M4/OneDrive/Documents/Programming\ Projects/STM32/STM32F466/STM32F4-BareMetal_Workspace/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+../Inc/uart.h:
+../Inc/adc.h:
+../Inc/systick.h:
---
+++ 11_systick_delay/Inc/systick.h
@@ -0,0 +1,13 @@
+/*
+ * systick.h
+ *
+ *  Created on: Feb 27, 2026
+ *      Author: Xx-M4
+ */
+
+#ifndef SYSTICK_H_
+#define SYSTICK_H_
+
+void systickDelayMs(int delay);
+
+#endif /* SYSTICK_H_ */
